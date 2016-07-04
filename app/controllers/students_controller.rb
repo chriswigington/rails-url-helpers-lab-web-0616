9178d3dca601893e0a5dd6b99ece21e2af20b119
chriswigington <chriswigington@gmail.com>
2016-07-04 15:59:11 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -6,6 +6,17 @@ class StudentsController < ApplicationController
   end
 
   def show
+    @student = Student.find(params[:id])
+    @student = set_student
+  end
+
+  def activate
+    @student = Student.find(params[:id])
+    #puts "before switch: #{@student.active}"
+    @student.switch
+    @student.save
+    #puts "after switch: #{@student.active}"
+    redirect_to "/students/#{params[:id]}"
   end
 
   private
--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,17 @@ class Student < ActiveRecord::Base
   def to_s
     self.first_name + " " + self.last_name
   end
+
+  def switch
+    if self.active
+      #puts "was active: #{active}"
+      self.active = false
+      #puts "now inactive: #{active}"
+    else
+      #puts "was inactive: #{active}"
+      self.active = true
+      #puts "now active: #{active}"
+    end
+  end
+
 end
